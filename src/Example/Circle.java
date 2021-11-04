4517c061c21f7ecfd466ecc2b4f764c4e2c860ca
Demetriusz <dmytroyeromenko.iko@gmail.com>
2021-11-04 10:07:02 +0100

    04.11.2021 added new solved tasks

--- src/Example/Circle.java
+++ src/Example/Circle.java
@@ -1,19 +1,23 @@
 package Example;
-import java.util.Scanner;
 
+import java.util.Scanner;
 import static java.lang.System.out;
 
 public class Circle {
+
     public static void main (String[] args) {
-        for( ; ; ) {
-            out.println("Insert circle's diameter in order to know circle's area and perimeter.");
+
+        do {
+            out.println("Insert circle's diameter in order to know circle's area, circumference and perimeter.");
             Scanner Scan1 = new Scanner(System.in);
-            double Diameter = Scan1.nextShort();
-            Double Area = (Math.PI * (Math.pow(Diameter, 2))) / 4;
-            Double Perimeter = (Math.PI * 2) * (Diameter / 2);
-            out.println("Circle's area = " + Area);
-            out.println("Circle's perimeter = " + Perimeter);
-        }
-        //Math.exp(Diameter * Math.log(2));
+            float diameter = Scan1.nextFloat();
+            float area = (float) (Math.PI * (Math.pow(diameter, 2))) / 4;
+            float perimeter = (float) ((Math.PI * 2) * (diameter / 2));
+            float circumference = (float) (Math.PI * diameter);
+            // L = πD = 2πr
+            out.println("Circle's area = " + area);
+            out.println("Circle's perimeter = " + perimeter);
+            out.println("Cirlce's circumference = " + circumference);
+        } while (true);
     }
 }
